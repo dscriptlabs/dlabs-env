6cf9f3580b9f1614198b9857dac9580e9d8e5dfa
FASami <foysal.developer@gmail.com>
2025-03-01 12:52:06 +0600

    refactor(version): read version directly from package.json
    - Remove separate version.js file
    - Use Node.js path and fs utilities to read package.json
    - Dynamically extract version for CLI command

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,19 @@
 import { Command } from 'commander'
 import { checkStatus } from './commands/index.js'
 import { initCommand } from './commands/init.js'
-import { version } from './version.js'
+import { readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const pkg = JSON.parse(readFileSync(join(__dirname, 'package.json'), 'utf8'))
 
 const program = new Command()
 
 program
   .name('dlabs-env')
   .description('CLI tool for environment management')
-  .version(version)
+  .version(pkg.version)
 
 program.addCommand(checkStatus)
 program.addCommand(initCommand)
