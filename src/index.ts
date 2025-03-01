7e52fe919d9c3653ec35eebe4fd80bf60f2c66d0
FASami <foysal.developer@gmail.com>
2025-03-01 13:41:14 +0600

    feat(cli): add init and status commands to program
    - Import and register init and status commands in main CLI entry point
    - Expand program configuration with new command handlers

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { Command } from 'commander'
 import { readFileSync } from 'fs'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
+import { initCommand } from './commands/init'
+import { checkStatus } from './commands/status'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const pkg = JSON.parse(readFileSync(join(__dirname, 'package.json'), 'utf8'))
@@ -15,4 +17,6 @@ program
   .description('CLI tool for environment management')
   .version(pkg.version)
 
+program.addCommand(initCommand)
+program.addCommand(checkStatus)
 program.parse()
