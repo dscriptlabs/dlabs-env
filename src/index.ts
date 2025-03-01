516b607639ad9faa96069a87eaa25ba5a7552346
FASami <foysal.developer@gmail.com>
2025-03-01 11:44:40 +0600

    feat(module): migrate to ES modules and improve version management
    - Add `type: "module"` to package.json
    - Update tsconfig to use Node16 module resolution
    - Create separate version.ts for dynamic version loading
    - Update import paths to use .js extensions
    - Include package.json and package-lock.json in published files

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander'
-import { checkStatus } from './commands'
-import { initCommand } from './commands/init'
-import { readFileSync } from 'fs'
-import { join } from 'path'
-
-const packageJson = JSON.parse(
-  readFileSync(join(__dirname, '..', 'package.json'), 'utf8'),
-)
+import { checkStatus } from './commands/index.js'
+import { initCommand } from './commands/init.js'
+import { version } from './version.js'
 
 const program = new Command()
 
 program
   .name('dlabs-env')
   .description('CLI tool for environment management')
-  .version(packageJson.version)
+  .version(version)
 
 program.addCommand(checkStatus)
 program.addCommand(initCommand)
